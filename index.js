b82db5029973d1aa35d84a7897156c15834d14e8
Andrew <66240551+dumpydev@users.noreply.github.com>
2022-05-13 12:01:16 +1000

    Update index.js

--- index.js
+++ index.js
@@ -13,10 +13,18 @@ function multiplyFrac() {
     return ((a / b) * (c / d)).toFixed(0)
 }
 function checkFraction() {
+    var a = document.getElementById("numerator").value
+    var b = document.getElementById("denominator").value
+    var c = document.getElementById("numerator2").value
+    var d = document.getElementById("denominator2").value
     if (multiplyFrac() != addFrac()) {
         document.getElementById("result").innerHTML = "OUTPUT: NOT IDEAL"
+        document.getElementById("addition").innerHTML = `Added: ${(a / b + c / d).toFixed(2)}`
+        document.getElementById("multiplication").innerHTML = `Multiplied: ${(a / b * c / d).toFixed(2)}`
     } else {
         document.getElementById("result").innerHTML = "OUTPUT: IDEAL"
+        document.getElementById("addition").innerHTML = `Added: ${(a / b + c / d).toFixed(2)}`
+        document.getElementById("multiplication").innerHTML = `Multiplied: ${(a / b * c / d).toFixed(2)}`
     }
 }
 document.addEventListener("keypress", function (event) {
